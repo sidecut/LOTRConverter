5ba5b8ad0113f15de18d4f9071f753cbcaf31322
Jim Raden <james.raden@gmail.com>
2024-12-11 17:13:47 -0500

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by James Raden on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
