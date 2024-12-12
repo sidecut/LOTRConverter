b57f6248fb50c23213d3b3d8c97384cc7baf6e0b
Jim Raden <james.raden@gmail.com>
2024-12-12 18:43:21 -0500

    Create computed properties for Currency enum

--- LOTRConverter/Currency.swift
+++ LOTRConverter/Currency.swift
@@ -5,7 +5,7 @@
 //  Created by James Raden on 12/12/24.
 //
 
-import Foundation
+import SwiftUI
 
 /// All values are in terms of a copperPenny
 enum Currency: Double {
@@ -14,4 +14,29 @@ enum Currency: Double {
     case silverPiece = 400
     case goldPenny = 1600
     case goldPiece = 6400
+
+    var image: ImageResource {
+        switch self {
+        case .copperPenny: .copperpenny
+        case .silverPenny: .silverpenny
+        case .silverPiece: .silverpiece
+        case .goldPenny: .goldpenny
+        case .goldPiece: .goldpiece
+        }
+    }
+
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
 }
