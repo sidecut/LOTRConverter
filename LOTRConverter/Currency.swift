f6636cc2c9cf1b1127357f956185fdc6001e2e65
Jim Raden <james.raden@gmail.com>
2024-12-12 18:26:06 -0500

    Add Currency enum that's actually the reciprocal of what he's doing

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,17 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by James Raden on 12/12/24.
+//
+
+import Foundation
+
+/// All values are in terms of a copperPenny
+enum Currency: Double {
+    case copperPenny = 1
+    case silverPenny = 100
+    case silverPiece = 400
+    case goldPenny = 1600
+    case goldPiece = 6400
+}
