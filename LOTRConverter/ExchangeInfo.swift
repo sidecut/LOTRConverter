533ca7911f3e6fbda17175b7fecfe51b3bfe0101
Jim Raden <james.raden@gmail.com>
2024-12-12 14:12:08 -0500

    Build out ExchangeInfo

--- LOTRConverter/ExchangeInfo.swift
+++ LOTRConverter/ExchangeInfo.swift
@@ -9,7 +9,49 @@ import SwiftUI
 
 struct ExchangeInfo: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image(.parchment)
+                .resizable()
+                .ignoresSafeArea()
+                .background(.brown)
+            
+            VStack {
+                Text("Exchange Rates")
+                    .font(.largeTitle)
+                    .tracking(3)
+                
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .padding(.top, -1.0)
+                    .padding(.leading, 30.0)
+                    .padding(.trailing, 30.0)
+                    .padding(.bottom)
+                
+                HStack{
+                    Image(.goldpiece)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33.0)
+                    
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                    
+                    Image(.goldpenny)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33.0)
+                }
+                
+                Button("Done") {
+                    
+                }
+                .buttonStyle(.borderedProminent)
+                .tint(.brown.mix(with: .black, by: 0.2))
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(.white)
+            }
+            .foregroundStyle(.black)
+        }
+        
     }
 }
 
